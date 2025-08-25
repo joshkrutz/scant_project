9bbd973056697251e32efdf682fcbee52825084f
OverKoffee <redmaldeveloper@gmail.com>
2025-08-25 14:58:48 -0700

    refactored homepage

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home.jsx";
-import { Layout } from "./pages/layout.jsx";
+import { Layout } from "./pages/Layout.jsx";
 import { NoPage } from "./pages/NoPage.jsx";
 import { ThemeProvider } from "./components/ThemeProvider.jsx";
 

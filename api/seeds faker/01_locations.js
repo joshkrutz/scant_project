fa55d64f808fd5ef5eb6ebb721bc908628b63a31
amarantino159 <amarantino159@gmail.com>
2025-08-25 13:57:46 -0700

    no facker data

---
+++ api/seeds faker/01_locations.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const { faker } = require('@faker-js/faker');
+// import { faker } from '@faker-js/faker';
+
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+
+  var data =[];
+  for(var i =0;i<100;i++){
+    var mock ={ Lat: faker.location.latitude(),Long: faker.location.longitude(),'Mailing Address': faker.location.streetAddress()+' '+faker.location.city()+' '+faker.location.state()+' '+faker.location.zipCode(), Country: 'US'};
+    data.push(mock);
+  }
+  await knex('locations').del()
+  await knex('locations').insert(
+    data
+  );
+};

d1bfe63f20a7a6fa18bad43ec0ed11f77378869f
Zak Chapman <zakchapman@rocketmail.com>
2025-08-25 09:44:04 -0600

    made skeleton for homepage

---
+++ ui/src/pages/NoPage.jsx
@@ -0,0 +1,7 @@
+export function NoPage() {
+  return (
+    <>
+      <h1>Page Not Found</h1>
+    </>
+  );
+}
--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,5 +8,5 @@ export default defineConfig({
     port: 3000,
     host: "0.0.0.0",
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 });

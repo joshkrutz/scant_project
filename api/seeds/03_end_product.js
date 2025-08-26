ceebd4af9d6d28e7287d1f6d79f0654a89cd814b
amarantino159 <amarantino159@gmail.com>
2025-08-26 09:22:39 -0700

    made the end_product table and refrences

---
+++ api/seeds/03_end_product.js
@@ -0,0 +1,26 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('end_product').del()
+  await knex('end_product').insert([
+    {
+      sat_name: 'GPS IIIF SV1',
+      mission: 'GPS',
+      generation: 'IIIF',
+      image_url: 'https://',
+      last_node_id: '3',
+    }
+
+  //   table.increments("id");
+  // table.string('sat_name');
+  // table.string('mission');
+  // table.string('generation');
+  // table.integer('last_node_id');
+  // table.foreign('last_node_id').references('player_node.id');
+  // table.string('image_url');
+
+]);
+};

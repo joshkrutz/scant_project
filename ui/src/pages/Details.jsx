8ebb9ba4b9f7a48f3e072e33c2b1d26056d24543
joshkrutz <jfkrutz@gmail.com>
2025-08-27 08:53:44 -0700

    Integrated home page

--- ui/src/pages/Details.jsx
+++ ui/src/pages/Details.jsx
@@ -1,19 +1,36 @@
 import { GraphToggle } from "../components/GraphToggle.jsx";
 import { DetailPanel } from "../components/DetailPanel.jsx";
 import { NodeGraph } from "../components/NodeGraph.jsx";
-import { useState } from "react";
+import { useParams } from "react-router-dom";
+import useSWR from "swr";
 
 export function Details() {
-  const [selectedNode, setSelectedNode] = useState("McRib");
+  const product_id = useParams().id;
+
+  // get product details
+  // TODO replace this with context variable
+  const { data, error, isLoading } = useSWR(
+    `http://localhost:8080/end_product/${product_id}`,
+    (url) =>
+      fetch(url)
+        .then((res) => res.json())
+        .then((json) => json[0])
+  );
+
+  if (isLoading) return <div>Is Loading...</div>;
+  if (error) return <div>failed to load</div>;
+
+  //const [selectedNode, setSelectedNode] = useState("McRib"); //use IDs with the
+  // graph equivalent of this display
   return (
     <div className="max-w-[800px] w-[75%] h-[100%] flex flex-col justify-start items-start">
       {/* <GraphToggle className="w-full bg-green-500 " /> */}
       <div className="flex flex-1 w-full">
         <NodeGraph
-          selectedNode={selectedNode}
-          setSelectedNode={setSelectedNode}
+          selectedNode={data.product_str}
+          setSelectedNode={data.product_str}
         />
-        <DetailPanel selectedNode={selectedNode} />
+        <DetailPanel selectedNode={data} />
       </div>
     </div>
   );
--- ui/src/pages/Home.jsx
+++ ui/src/pages/Home.jsx
@@ -3,61 +3,15 @@ import useSWR from "swr";
 import { Gallery } from "../components/Gallery.jsx";
 
 export function Home() {
-  // const {data, error, isLoading} = useSWR('/fill me in', fetcher);
-  const [data, setData] = useState();
-  const [error, setError] = useState();
-  const [isLoading, setisLoading] = useState(true);
+  const { data, error, isLoading } = useSWR(
+    "http://localhost:8080/end_product",
+    (url) =>
+      fetch(url)
+        .then((res) => res.json())
+        .then((json) => json)
+  );
 
-  useEffect(() => {
-    try {
-      setData([
-        {
-          id: 1,
-          product_str: "McRib",
-          quantity: 150,
-          image:
-            "https://s7d1.scene7.com/is/image/mcdonalds/DC_201910_0010_McRib_1564x1564-1:product-header-mobile?wid=1313&hei=1313&dpr=off",
-          mission: "Feed People",
-          player_node: 1,
-        },
-        {
-          id: 2,
-          product_str: "BigMac",
-          quantity: 150,
-          image:
-            "https://s7d1.scene7.com/is/image/mcdonalds/DC_201910_0010_McRib_1564x1564-1:product-header-mobile?wid=1313&hei=1313&dpr=off",
-          mission: "Feed People",
-          player_node: 1,
-        },
-        {
-          id: 3,
-          product_str: "McChicken",
-          quantity: 150,
-          image:
-            "https://s7d1.scene7.com/is/image/mcdonalds/DC_201910_0010_McRib_1564x1564-1:product-header-mobile?wid=1313&hei=1313&dpr=off",
-          mission: "Feed People",
-          player_node: 1,
-        },
-        {
-          id: 4,
-          product_str: "McFlurry",
-          quantity: 150,
-          image:
-            "https://s7d1.scene7.com/is/image/mcdonalds/DC_201910_0010_McRib_1564x1564-1:product-header-mobile?wid=1313&hei=1313&dpr=off",
-          mission: "Feed People",
-          player_node: 1,
-        },
-      ]);
-    } catch (err) {
-      setError(err);
-    } finally {
-      setTimeout(() => {
-        setisLoading(false);
-      }, 1000);
-    }
-  }, []);
   if (error) return <div>failed to load</div>;
-
   return (
     <>
       <Gallery
